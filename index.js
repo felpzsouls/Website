794ea45823108103c0fa4d4597290eb0b58d4de4
Felpzsouls <87570837+felpzsouls@users.noreply.github.com>
2024-02-24 10:52:48 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Felpzsouls</title>
+    <link rel="stylesheet" href="./css/index.css" />
+  </head>
+  <body>
+    <nav class="nav">
+      <div class="navbar">
+        <div class="navbar-content">
+          <div class="brand">
+            <a href="./index.html">
+              <h2>Felpzsouls</h2>
+            </a>
+          </div>
+          <div class="contacts">
+            <button class="ctt-btn" onclick="cttBtn()">Contatos</button>
+            <div class="ctt" id="ctt">
+              <ul>
+                <li><a href="https://github.com/Felpzsoul">GitHub</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+
+    <script src="./scripts/index.js"></script>
+  </body>
+</html>
