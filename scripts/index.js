eefb71a11f82d0d7b0539cc3ec72cb7ef87a0d4f
Felpzsouls <87570837+felpzsouls@users.noreply.github.com>
2024-02-24 10:53:09 -0300

    Create index.js

---
+++ scripts/index.js
@@ -0,0 +1,9 @@
+function cttBtn() {
+    let ctt = document.getElementById('ctt');
+
+    if(ctt.style.display === 'none') {
+        ctt.style.display = 'block';
+    } else {
+        ctt.style.display = 'none';
+    }
+}
